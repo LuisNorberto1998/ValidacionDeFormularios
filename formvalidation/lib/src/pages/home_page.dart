6b44aacbccd34769461b72222884e9a06fc1bec5
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-05-27 19:49:15 -0500

    Mantener data de streams después de un hot reload

--- formvalidation/lib/src/pages/home_page.dart
+++ formvalidation/lib/src/pages/home_page.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/blocs/provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Center(child: Text('Home Page')),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text('Email: ${bloc.email}'),
+          Divider(),
+          Text('Password: ${bloc.password}'),
+        ],
       ),
     );
   }
